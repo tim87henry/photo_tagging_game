57c1a815fdd2c294e083725ae155baf552840023
Timothy Henry <tim87henry@gmail.com>
2021-10-01 23:26:32 +1000

    Added function to validate user selection

--- src/App.js
+++ src/App.js
@@ -3,12 +3,42 @@ import Game from "./Game.js";
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 
 function App() {
+
+  const checkSelection = (x,y,character) => {
+    console.log("The corodinates you selected are "+x+" "+y);
+    let correctSelection = false;
+    let xMin, xMax, yMin, yMax = 0;
+    if (character === 1) {
+      xMin = 645;
+      xMax = 740;
+      yMin = 1310;
+      yMax = 1430;
+      console.log("Trinity")
+    } else if (character === 2) {
+      xMin = 210;
+      xMax = 280;
+      yMin = 700;
+      yMax = 870;
+      console.log("Sarah")
+    } else {
+      xMin = 280;
+      xMax = 300;
+      yMin = 1300;
+      yMax = 1330;
+      console.log("Astro")
+    }
+
+    correctSelection = (x >= xMin && x <= xMax && y >= yMin && y <= yMax)? true : false;
+    console.log("That's "+correctSelection)
+
+  }
+
   return (
     <div className="App">
       <Navbar />
       <BrowserRouter>
         <Switch>
-          <Route path="/game" render={() => <Game />}></Route>
+          <Route path="/game" render={() => <Game checkSelection={checkSelection} />}></Route>
           <p>
             Click <Link to="/game">Here</Link>
           </p>
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,9 @@
 import fight_club from "./data/fight_club.png";
 import "./style.css";
 
-const Game = () => {
+const Game = (props) => {
 
-    const checkClick = (e) => {
+    const getCoords = (e) => {
         let oElement = e.target;
         let ImgPos=[];
         if(typeof(oElement.offsetParent ) != "undefined")
@@ -35,12 +35,23 @@ const Game = () => {
         }
         PosX = PosX - ImgPos[0];
         PosY = PosY - ImgPos[1];
-        console.log("Position is "+PosX+" , "+PosY)
+        return [PosX, PosY]
+    }
+
+    const showMenu = () => {
+        // Display menu to select character
+    }
+
+    const processClick = (e) => {
+        let PosX = getCoords(e)[0];
+        let PosY = getCoords(e)[1];
+        showMenu();
+        props.checkSelection(PosX,PosY,3);
     }
 
     return (
         <div className="imageDiv">
-            <img src={fight_club} alt="" useMap="#gamemap" className="gameImage" onClick={checkClick}></img>
+            <img src={fight_club} alt="" useMap="#gamemap" className="gameImage" onClick={processClick}></img>
             <map name="gamemap">
                 {/* <area shape="poly" coords="280,1300, 300,1300, 300,1330, 280,1330" href="google.com"></area> */}
             </map>
