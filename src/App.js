4c302b4f8c0da0d3bae26179fff69cb82d7c8bee
Timothy Henry <tim87henry@gmail.com>
2021-10-11 21:15:26 +1100

    Added player choice validation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,62 @@
 import Navbar from "./Navbar.js";
 import Game from "./Game.js";
 import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
+import {useState} from "react";
 
 function App() {
 
+  const [characters,setCharacters] = useState(
+    [{
+      "name": "Trinity",
+      "found": false
+    },{
+      "name": "Sarah Connor",
+      "found": false
+    },{
+      "name": "Astro Boy",
+      "found": false
+    }]);
+
   const checkSelection = (x,y,character) => {
-    console.log("The corodinates you selected are "+x+" "+y);
+    
     let correctSelection = false;
     let xMin, xMax, yMin, yMax = 0;
+    let charactersCopy = [];
+    
     if (character === 1) {
       xMin = 645;
       xMax = 740;
       yMin = 1310;
       yMax = 1430;
-      console.log("Trinity")
+      charactersCopy.push({
+        "name": "Trinity", 
+        "found": true
+      },characters[1],characters[2]);
     } else if (character === 2) {
       xMin = 210;
       xMax = 280;
       yMin = 700;
       yMax = 870;
-      console.log("Sarah")
+      charactersCopy.push(characters[0],{
+        "name": "Sarah Connor",
+        "found": true
+      },characters[2]);
     } else {
       xMin = 280;
       xMax = 300;
       yMin = 1300;
       yMax = 1330;
-      console.log("Astro")
+      charactersCopy.push(characters[0],characters[1],{
+        "name": "Astro Boy",
+        "found": true
+      });
     }
 
     correctSelection = (x >= xMin && x <= xMax && y >= yMin && y <= yMax)? true : false;
-    console.log("That's "+correctSelection)
+    if (correctSelection) {
+      setCharacters(charactersCopy);
+    }
+    
     const menus = document.getElementsByClassName("menuDiv");
     for (let i=0;i<menus.length;i++) {
         menus[i].style.display = "none";
@@ -38,7 +65,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar characters={characters} />
       <BrowserRouter>
         <Switch>
           <Route path="/game" render={() => <Game checkSelection={checkSelection} />}></Route>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,20 @@
-const Navbar = () => {
+const Navbar = (props) => {
+    
+    let charClassName = [];
+    for (let i=0;i<3;i++) {
+        if (props.characters[i].found) {
+            charClassName.push("found");
+        } else {
+            charClassName.push("notFound");
+        }
+    }
+
     return(
-        <div>
-            <ul>
-                <li>Trinity</li>
-                <li>Sarah Connor</li>
-                <li>Astro Boy</li>
+        <div key={props.characters}>
+            <ul className="charList">
+                <li className={charClassName[0]}>{props.characters[0].name}</li>
+                <li className={charClassName[1]}>{props.characters[1].name}</li>
+                <li className={charClassName[2]}>{props.characters[2].name}</li>
             </ul>
         </div>
     );
